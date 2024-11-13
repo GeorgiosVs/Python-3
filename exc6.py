1b648cc417dd9f8ef42a059482b86fd1b2770175
GeorgiosVs <jinje1555@gmail.com>
2024-11-13 18:13:28 +0200

    Python 3 revisited 13/11/24

--- exc6.py
+++ exc6.py
@@ -1,1 +1,15 @@
 #Version 1 13/11/2024
+name = 'Mariza'
+
+def sayHello():
+    print('Hello, ' + name)
+
+def changeName(newName):
+    global name
+    name = newName
+
+sayHello()
+changeName('Juan')
+sayHello()
+changeName('George')
+sayHello()
