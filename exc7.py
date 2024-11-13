f0810c6cfb1644e0d054130c3bd7866819415925
GeorgiosVs <jinje1555@gmail.com>
2024-11-13 20:01:54 +0200

    Testing

--- exc7.py
+++ exc7.py
@@ -1,7 +1,21 @@
-#Version 1 updated through
+""" Version 1 updated through
 def func(a, b=1, c=2):
     print('a =', a, 'και b =', b, 'και c =', c)
 
 func(5, 10)
 func(2, c = 20)
-func(c = 50, a = 100)
+func(c = 50, a = 100)"""
+
+import exc8
+
+myNumber1 = 30
+myNumber2 = 20
+
+print("Your first number is ", myNumber1)
+print("Your second number is ", myNumber2)
+
+print("Append ", exc8.append(myNumber1, myNumber2))
+
+print("Division ", exc8.division(myNumber1, myNumber2))
+
+print("Multiply ",exc8.multiply(myNumber1, myNumber2))
--- exc8.py
+++ exc8.py
@@ -1,7 +1,16 @@
 #Version 1
-def myFunc(word):
+"""def myFunc(word):
     return word[::-1]
 
 user_input = input("Enter a word:")
 
-print (myFunc(user_input))
+print (myFunc(user_input))"""
+
+def append(x, y):
+    return x + y
+
+def division(x, y):
+    return x / y
+
+def multiply(x, y):
+    return x * y
